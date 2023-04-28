2d5e6e68fb5a6606b69c0913ba71a658627283e8
davleo <david.amadoral@gmail.com>
2023-04-28 16:09:57 -0500

    task-03

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,61 @@
+/*Tarea 3
+
+Escriba un script para crear una galería de imágenes a partir de un array de datos. El HTML tiene una lista ul.gallery.
+
+<ul class="gallery"></ul>
+
+Use un array de objetos images para crear elementos <img> anidados dentro de <li>. Use las secuencias de plantillas y el método insertAdjacentHTML() para crear el marcado.
+
+Todos los elementos de la galería deben añadirse al DOM en una sola operación de inserción.
+Añada un diseño mínimo a la galería con flexboxes o Computación en malla mediante las clases CSS.
+
+*/
+
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+const gallery = document.querySelector(".gallery");
+
+const galleryMarkup = images
+  .map(
+    (image) => `
+  <li>
+    <img src="${image.url} alt="${image.alt}">
+  </li> 
+`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const style = document.createElement("style");
+
+style.textContent = `
+  .gallery {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    list-style: none;
+    padding: 0;
+  }
+  .gallery li {
+    margin-bottom: 20px;
+  }
+  .gallery img {
+    max-width: 100%;
+  }
+`;
+
+document.head.appendChild(style);
